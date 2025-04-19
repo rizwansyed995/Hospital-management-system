b4d01997086ae707d1ce78b7a120673dc2a420c8
Syed Rizwan <rizwansyed995@gmail.com>
2025-04-19 22:17:31 +0530

    backend for user profile

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,13 @@
 import express from 'express'
-import { registerUser, verifyEmail,loginUser } from "../controller/userController.js";
-
+import { registerUser, verifyEmail,loginUser, getProfile, updateProfile } from "../controller/userController.js";
+import authUser from '../middleware/authUser.js'
+import upload from '../middleware/multer.js';
 
 const userRouter = express.Router()
 
 userRouter.post('/register', registerUser)
 userRouter.post("/verify-email", verifyEmail);
 userRouter.post("/login", loginUser);
+userRouter.get("/get-profile" ,authUser , getProfile);
+userRouter.post("/update-profile",upload.single('image'),authUser ,updateProfile);
 export default userRouter
